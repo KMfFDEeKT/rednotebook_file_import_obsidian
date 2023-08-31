85de529b100f749627e4896293f84e5abad2f691
KMfFDEeKT <143703011+KMfFDEeKT@users.noreply.github.com>
2023-08-31 11:47:22 +0200

    Update rednotebook_diary_import_obsidian.py

--- rednotebook_diary_import_obsidian.py
+++ rednotebook_diary_import_obsidian.py
@@ -18,6 +18,6 @@ for i in new_text:
     form_name = name[0].replace('\n', '').replace(',', '').replace('/', '-')
     form_name = form_name[1:11]
     print(form_name)
-    with open(f'/home/omd/Python/output_files/diary_import/{form_name}.md', 'a+') as f:
+    with open(f'/home/Desktop/Downloads/{form_name}.md', 'a+') as f:
         f.write(i)
 
